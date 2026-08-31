7d14dbb37d33d89d80ebea58883f5cbd303acc66
Numan <33553832+Numi2@users.noreply.github.com>
2026-08-31 02:05:01 +0200

    Use deployment-aware Kingfisher base URL

--- baseUrl.js
+++ baseUrl.js
@@ -1,1 +1,3 @@
-export const baseURL = "";
+const vercelHost = process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL;
+
+export const baseURL = process.env.NEXT_PUBLIC_BASE_URL || (vercelHost ? `https://${vercelHost}` : "http://localhost:3000");
